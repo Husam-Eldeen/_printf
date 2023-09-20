b3a4ebe552c83aba9752d83e653e048442fb814a
markouny <a_markony@hotmail.com>
2023-09-19 17:39:52 -0700

    debugg

--- main.h
+++ main.h
@@ -1,9 +1,8 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef MAIN_H_
+#define MAIN_H_
 #include <stdarg.h>
 #include <string.h>
 #include <unistd.h>
-#include <stdio.h>
 #include <stdlib.h>
 
 int _printf(const char *format, ...);
